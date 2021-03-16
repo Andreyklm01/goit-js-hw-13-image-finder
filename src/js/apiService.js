7b9d7b36e4b946cfc19696c6fc0bfcc375bf3f5f
Andrey Klemenko <web.klemenko@gmail.com>
2021-03-17 00:06:57 +0200

    add function markupCard

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -7,14 +7,15 @@ async function apiService(searchQuery) {
   try {
     const searchList = await fetch(
       `${BASE_URL}&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}`,
-    );
-    return searchList.json();
+    )
+      .then(result => result.json())
+      .then(data => data.hits);
+    //нужно вытянуть нужные параметры из хитс
+    return searchList;
   } catch (error) {
-    throw error;
+    console.log(error);
   }
 }
-const test = apiService('cat');
-console.log(test);
 
 const galleryRef = document.querySelector('.gallery');
 const formRef = document.querySelector('#search-form');
@@ -24,9 +25,19 @@ formRef.addEventListener('submit', event => {
   event.preventDefault();
 
   console.log(inputRef.value);
-
+  // не рендерется шаблон
+  const query = apiService(inputRef.value);
+  markupCard(query);
   formRef.reset();
 });
+
+function markupCard(value) {
+  const card = cardTmpl(value);
+
+  const addElement = document.createElement('li');
+  addElement.insertAdjacentHTML('beforeend', card);
+  galleryRef.appendChild(addElement);
+}
 
 //Если промис возвращает true, зарендерить разметку каждого в кард.
 // Затем зарендерить в ul галерею
